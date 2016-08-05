e74e533d9a37c0903e253f85fce5c11cee39838c
James Paskett <jpaskett@Jamess-MacBook-Air.local>
2016-08-05 19:14:16 -0400

    Add Problem Set 0 Answers

---
+++ ps0.py
@@ -0,0 +1,9 @@
+# Problem Set 0
+# Name: James Paskett
+# Collaborators: None
+# Time Spent : < 1
+
+dob = raw_input('Enter your date of birth:')
+last_name = raw_input('Enter your last name:')
+
+print last_name + " " + dob
